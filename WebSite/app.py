a002b627ba26b63567bb4ea96271c8b5c796b978
yahiashamaa <yehiashamaa987@gmail.com>
2023-10-06 01:12:51 +0300

    login

--- WebSite/app.py
+++ WebSite/app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request , redirect
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 @app.route("/")
 def index():
+    return render_template('welcome.html')
+
+@app.route("/login")
+def login():
     return render_template('login.html')
+
+
+
+@app.route("/signup")
+def signup():
+    return render_template('register.html')
+
